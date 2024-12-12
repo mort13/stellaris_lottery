183eb8fb523003a41ea8564b607ac583754fbd48
mort13 <mo@redback.de>
2024-12-12 11:58:09 +0100

    Added support for the mod evolved

--- Stellaris_lottery/main.py
+++ Stellaris_lottery/main.py
@@ -9,16 +9,15 @@ import functions
 import names
 
 gestalt = names.gestalt_num
-player_num = len(names.players) - gestalt
 
 
-vectors_1, vectors_2 = functions.create_ethic_vectors()
-vectors = np.vstack((vectors_1,vectors_2))
-
+vectors = functions.create_ethic_vectors(names.ethic_axis,5)
+print(len(vectors))
 ethic_vectors = functions.assign_ethics_to_vector(vectors, names.ethic_axis,gestalt)
 
-ethic_pairing = functions.assign_ethic_to_player(names.players, ethic_vectors,names.ethic_num)
+ethic_pairing = functions.assign_ethic_to_player(names.players, ethic_vectors,names.ethic_num,gestalt)
 
 ethic_origin_pairing = functions.assign_origins_to_players(ethic_pairing, names.origin_ethics_mapping)
 
 functions.write_out(ethic_origin_pairing)
+
